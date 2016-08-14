5ee2f6e3292c19744e3843450cd595193b02bde9
Ivan Nikolić <niksy5@gmail.com>
2016-08-14 10:27:28 +0200

    rename "automatic" to "automated"
    Fixes #38.

--- generators/app/templates/test/automated/index.js
+++ generators/app/templates/test/automated/index.js
@@ -3,7 +3,7 @@
 	var html = document.getElementsByTagName('html')[0];
 
 	before(function () {
-		var fixture = window.__html__['test/automatic/fixtures/index.html'];
+		var fixture = window.__html__['test/automated/fixtures/index.html'];
 		document.body.insertAdjacentHTML('beforeend', '<div id="fixture">' + fixture + '</div>');
 	});
 
